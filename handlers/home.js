575bfae9feb6c7e44f4b802f256aaf01127ae047
tormod17 <tormodsmith@gmail.com>
2015-10-21 13:36:54 +0100

    adding to handlers

--- handlers/home.js
+++ handlers/home.js
@@ -1,5 +1,10 @@
+var fs =require('fs');
+var index = fs.readFileSync('/home/tormod/FAC6/tuber/public/index.html')
+
+
+
 module.exports = function home(req, res){
   res.writeHead(200, {'Content-Type':'text/html'});
-  res.end('hello world');
+  res.end(index);
 };
 
--- handlers/journeyApi.js
+++ handlers/journeyApi.js
@@ -1,8 +1,30 @@
 var env = require('env2')('./config.env');
+var http = require('http');
 
-module.exports = function home(req, res){
-  res.writeHead(200, {'Content-Type':'text/html'});
+module.exports = function JourneyApi(req, res) {
+    console.log('JOURNEY API');
 
-  res.end();
+    /*var options = {
+        hostname:'api.tfl.gov.uk',
+        port: 80,
+        path: '/Journey/JourneyResults/Bethnal%20Green%20Underground%20Station/to/Liverpool%20Street%20Underground%20Station?nationalSearch=False&&&timeIs=Departing&&mode=tube&&&&&&&&&&&alternativeCycle=False&alternativeWalking=True&applyHtmlMarkup=False&useMultiModalCall=False&app_id=22e08f6b&app_key=c16e4ee45d8253d4e2e7649174057f08',
+        method: 'GET'
+    };
+
+
+    callback = function(repsonse) {
+        var str = '';
+        console.log('JOURNEY API CB');
+        res.on('data', function(chunk) {
+            console.log (chunk);
+            str += chunk;
+        });
+        res.on('end', function() {
+            var data =JSON.parse(str);
+            console.log(data);
+        });
+    }
+
+    http.request(options, callback).end();*/
+
 };
-
